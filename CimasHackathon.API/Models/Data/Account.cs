a8d4992a76ed7f20ef953d7e6b3099ab0475bbe8
Keith Stanley <iamstan13y@gmail.com>
2022-09-26 01:37:26 +0200

    Account & Patient model + DB migrations.

--- CimasHackathon.API/Models/Data/Account.cs
+++ CimasHackathon.API/Models/Data/Account.cs
@@ -5,11 +5,9 @@ namespace CimasHackathon.API.Models.Data
     public class Account
     {
         public int Id { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
         public string? Email { get; set; }
         public string? Password { get; set; }
-        public int RoleId { get; set; }
+        public string? PhoneNumber { get; set; }
         public DateTime DateCreated { get; set; } = DateTime.Now;
         [NotMapped]
         public string? Token { get; set; }
