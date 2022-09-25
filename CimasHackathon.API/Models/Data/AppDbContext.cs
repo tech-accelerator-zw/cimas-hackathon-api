4d491cc1a8bc32492e80d636d560da0af042d92b
Keith Stanley <iamstan13y@gmail.com>
2022-09-25 23:14:09 +0200

    Config rhetoric - I.

---
+++ CimasHackathon.API/Models/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CimasHackathon.API.Models.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Account>? Accounts { get; set; }
+        public DbSet<GeneratedCode>? GeneratedCodes { get; set; }
+    }
+}
