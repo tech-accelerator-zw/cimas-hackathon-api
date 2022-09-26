ae8308f028bd1f5e7b511cb254b1ea0581dd9ba6
Keith Stanley <iamstan13y@gmail.com>
2022-09-26 15:55:26 +0200

    Get medication by disease Id.

--- CimasHackathon.API/Models/Repository/MedicationRepository.cs
+++ CimasHackathon.API/Models/Repository/MedicationRepository.cs
@@ -1,5 +1,7 @@
 ﻿using CimasHackathon.API.Models.Data;
+using CimasHackathon.API.Models.Local;
 using CimasHackathon.API.Models.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
 
 namespace CimasHackathon.API.Models.Repository
 {
@@ -7,6 +9,13 @@ namespace CimasHackathon.API.Models.Repository
     {
         public MedicationRepository(AppDbContext context) : base(context)
         {
+        }
+
+        public async Task<Result<IEnumerable<Medication>>> GetByDiseaseIdAsync(int diseaseId)
+        {
+            var medications = await _dbSet.Where(x => x.DiseaseId == diseaseId).ToListAsync();
+
+            return new Result<IEnumerable<Medication>>(medications);
         }
     }
 }
