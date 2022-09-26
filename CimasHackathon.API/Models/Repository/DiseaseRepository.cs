bd718e6a73605ed6f4dd6a2075a5545d21558a6b
Keith Stanley <iamstan13y@gmail.com>
2022-09-26 12:34:30 +0200

    Diseases CRUD.

---
+++ CimasHackathon.API/Models/Repository/DiseaseRepository.cs
@@ -0,0 +1,24 @@
+﻿using CimasHackathon.API.Models.Data;
+using CimasHackathon.API.Models.Local;
+using CimasHackathon.API.Models.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace CimasHackathon.API.Models.Repository
+{
+    public class DiseaseRepository : Repository<Disease>, IDiseaseRepository
+    {
+        public DiseaseRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<Result<IEnumerable<Disease>>> GetByMedicationIdAsync(int medicationId)
+        {
+            var diseases = await _dbSet
+                .Where(x => x.MedicationId == medicationId)
+                .Include(x => x.Medication)
+                .ToListAsync();
+
+            return new Result<IEnumerable<Disease>>(diseases);
+        }
+    }
+}
