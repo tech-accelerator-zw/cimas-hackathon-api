a73c8e6682e3d90e3a26cbcf7357e56a23857f08
Keith Stanley <iamstan13y@gmail.com>
2022-09-26 11:30:13 +0200

    Medication model & DB migrations.

---
+++ CimasHackathon.API/Migrations/20220926092941_MedicationToDb.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CimasHackathon.API.Migrations
+{
+    public partial class MedicationToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PhoneNumber",
+                table: "Accounts",
+                newName: "CimasNumber");
+
+            migrationBuilder.CreateTable(
+                name: "Medications",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medications", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Medications");
+
+            migrationBuilder.RenameColumn(
+                name: "CimasNumber",
+                table: "Accounts",
+                newName: "PhoneNumber");
+        }
+    }
+}
