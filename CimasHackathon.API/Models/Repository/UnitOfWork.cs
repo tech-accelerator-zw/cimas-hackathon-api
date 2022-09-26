028bbba0a12f9a7c47b5f7df7db0b2ee3fc965f8
Keith Stanley <iamstan13y@gmail.com>
2022-09-26 23:25:36 +0200

    Doctor CRUD - I.

--- CimasHackathon.API/Models/Repository/UnitOfWork.cs
+++ CimasHackathon.API/Models/Repository/UnitOfWork.cs
@@ -9,12 +9,14 @@ namespace CimasHackathon.API.Models.Repository
         public IPatientRepository Patient { get; private set; }
         public IMedicationRepository Medication { get; private set; }
         public IDiseaseRepository Disease { get; private set; }
-        
+        public IDoctorRepository Doctor { get; private set; }
+
         public UnitOfWork(AppDbContext context)
         {
             Patient = new PatientRepository(context);
             Medication = new MedicationRepository(context);
             Disease = new DiseaseRepository(context);
+            Doctor = new DoctorRepository(context);
             _context = context;
         }
 
