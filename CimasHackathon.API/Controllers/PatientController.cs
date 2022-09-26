d45ff4c1d9fcb73661daea50d650846283bdb124
Keith Stanley <iamstan13y@gmail.com>
2022-09-26 11:15:48 +0200

    Get All Patients impl.

---
+++ CimasHackathon.API/Controllers/PatientController.cs
@@ -0,0 +1,17 @@
+﻿using CimasHackathon.API.Models.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CimasHackathon.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class PatientController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public PatientController(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+
+        [HttpGet]
+        public async Task<IActionResult> Get() => Ok(await _unitOfWork.Patient.GetAllAsync());
+    }
+}
