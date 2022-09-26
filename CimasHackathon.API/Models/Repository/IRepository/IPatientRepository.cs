dee16a9daf929b2a55dc266e7d990eb4379424dc
Keith Stanley <iamstan13y@gmail.com>
2022-09-26 11:07:45 +0200

    Patient Repository & UoW.

---
+++ CimasHackathon.API/Models/Repository/IRepository/IPatientRepository.cs
@@ -0,0 +1,8 @@
+﻿using CimasHackathon.API.Models.Data;
+
+namespace CimasHackathon.API.Models.Repository.IRepository
+{
+    public interface IPatientRepository : IRepository<Patient>
+    {
+    }
+}
