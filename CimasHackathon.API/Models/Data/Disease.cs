716d6f46639ca240de271bef9d2f67f316936c01
Keith Stanley <iamstan13y@gmail.com>
2022-09-26 12:34:09 +0200

    Diseases CRUD.

---
+++ CimasHackathon.API/Models/Data/Disease.cs
@@ -0,0 +1,10 @@
+﻿namespace CimasHackathon.API.Models.Data
+{
+    public class Disease
+    {
+        public int Id { get; set; }
+        public int MedicationId { get; set; }
+        public string? Name { get; set; }
+        public Medication? Medication { get; set; }
+    }
+}
