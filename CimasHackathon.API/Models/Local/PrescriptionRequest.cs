20ce80a0c63842a39c2a65b3f0fb46d5bd52d097
Keith Stanley <iamstan13y@gmail.com>
2022-09-27 01:00:22 +0200

    Add Prescription Impl.

---
+++ CimasHackathon.API/Models/Local/PrescriptionRequest.cs
@@ -0,0 +1,12 @@
+﻿using CimasHackathon.API.Enums;
+
+namespace CimasHackathon.API.Models.Local
+{
+    public class PrescriptionRequest
+    {
+        public int PatientId { get; set; }
+        public List<int>? MedicationIds { get; set; }
+        public string? Description { get; set; }
+        public PrescriptionStatus Status { get; set; }
+    }
+}
