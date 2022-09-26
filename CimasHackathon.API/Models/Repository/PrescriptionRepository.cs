604b202f9136a2ea91fa3d512e39af1bc9ac5cb4
Keith Stanley <iamstan13y@gmail.com>
2022-09-27 00:11:33 +0200

    Prescriprion Repository & UoW.

---
+++ CimasHackathon.API/Models/Repository/PrescriptionRepository.cs
@@ -0,0 +1,12 @@
+﻿using CimasHackathon.API.Models.Data;
+using CimasHackathon.API.Models.Repository.IRepository;
+
+namespace CimasHackathon.API.Models.Repository
+{
+    public class PrescriptionRepository : Repository<Prescription>, IPrescriptionRepository
+    {
+        public PrescriptionRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
