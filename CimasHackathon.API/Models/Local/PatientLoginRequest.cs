87d0a48c056638003ad8cc8407dabd9349935431
Keith Stanley <iamstan13y@gmail.com>
2022-09-26 02:10:38 +0200

    Patient Account config.

---
+++ CimasHackathon.API/Models/Local/PatientLoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace CimasHackathon.API.Models.Local
+{
+    public class PatientLoginRequest
+    {
+        public string? Otp { get; set; }
+        public string? Email { get; set; }
+    }
+}
