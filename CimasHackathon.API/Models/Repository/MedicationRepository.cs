8f459bceedde457560f36ba265f9cf4342c46fef
Keith Stanley <iamstan13y@gmail.com>
2022-09-26 11:55:03 +0200

    Medication CRUD.

---
+++ CimasHackathon.API/Models/Repository/MedicationRepository.cs
@@ -0,0 +1,12 @@
+﻿using CimasHackathon.API.Models.Data;
+using CimasHackathon.API.Models.Repository.IRepository;
+
+namespace CimasHackathon.API.Models.Repository
+{
+    public class MedicationRepository : Repository<Medication>, IMedicationRepository
+    {
+        public MedicationRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
