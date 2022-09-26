3cae43e768f3dacfa1897d1d96272d24f3df291c
Keith Stanley <iamstan13y@gmail.com>
2022-09-27 01:51:38 +0200

    Search Medication by Name.

--- CimasHackathon.API/Models/Repository/IRepository/IMedicationRepository.cs
+++ CimasHackathon.API/Models/Repository/IRepository/IMedicationRepository.cs
@@ -6,5 +6,6 @@ namespace CimasHackathon.API.Models.Repository.IRepository
     public interface IMedicationRepository : IRepository<Medication>
     {
         Task<Result<IEnumerable<Medication>>> GetByDiseaseIdAsync(int diseaseId);
+        Task<Result<IEnumerable<Medication>>> SearchByNameAsync(string name);
     }
 }
