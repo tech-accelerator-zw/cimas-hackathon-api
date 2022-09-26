31e99e2d6f1efbf38798b680ca31db77293b206a
Keith Stanley <iamstan13y@gmail.com>
2022-09-26 12:34:19 +0200

    Diseases CRUD.

---
+++ CimasHackathon.API/Models/Repository/IRepository/IDiseaseRepository.cs
@@ -0,0 +1,10 @@
+﻿using CimasHackathon.API.Models.Data;
+using CimasHackathon.API.Models.Local;
+
+namespace CimasHackathon.API.Models.Repository.IRepository
+{
+    public interface IDiseaseRepository : IRepository<Disease>
+    {
+        Task<Result<IEnumerable<Disease>>> GetByMedicationIdAsync(int medicationId);
+    }
+}
