12b227f1bb42865db8ec247700719d79a8959796
Keith Stanley <iamstan13y@gmail.com>
2022-09-27 01:22:47 +0200

    Get Prescriptions By Status.

--- CimasHackathon.API/Models/Repository/IRepository/IPrescriptionRepository.cs
+++ CimasHackathon.API/Models/Repository/IRepository/IPrescriptionRepository.cs
@@ -1,4 +1,5 @@
-﻿using CimasHackathon.API.Models.Data;
+﻿using CimasHackathon.API.Enums;
+using CimasHackathon.API.Models.Data;
 using CimasHackathon.API.Models.Local;
 
 namespace CimasHackathon.API.Models.Repository.IRepository
@@ -8,6 +9,7 @@ namespace CimasHackathon.API.Models.Repository.IRepository
         Task<Result<IEnumerable<Prescription>>> GetByPatientIdAsync(int patientId);
         Task<Result<IEnumerable<Prescription>>> GetByCimasNumberAsync(string cimasNumber);
         Task<Result<IEnumerable<Prescription>>> GetByDoctorIdAsync(int doctorId);
+        Task<Result<IEnumerable<Prescription>>> GetByStatusAsync(PrescriptionStatus status);
         Task<Result<bool>> AddPrescriptionAsync(PrescriptionRequest request);
     }
 }
