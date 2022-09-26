b58e94d22438b1ec8d21cd9fff37b7cc5bd3adfa
Keith Stanley <iamstan13y@gmail.com>
2022-09-26 15:43:38 +0200

    Medication -> Disease relationship fix & DB migrations.

--- CimasHackathon.API/Models/Data/Disease.cs
+++ CimasHackathon.API/Models/Data/Disease.cs
@@ -3,8 +3,6 @@
     public class Disease
     {
         public int Id { get; set; }
-        public int MedicationId { get; set; }
         public string? Name { get; set; }
-        public Medication? Medication { get; set; }
     }
 }
