6988f35bc638df45c827a6bddb92ff0d3fa0a216
Keith Stanley <iamstan13y@gmail.com>
2022-09-26 23:25:18 +0200

    Doctor CRUD - I.

---
+++ CimasHackathon.API/Controllers/DoctorController.cs
@@ -0,0 +1,26 @@
+﻿using CimasHackathon.API.Models.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CimasHackathon.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class DoctorController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DoctorController(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+
+        [HttpGet]
+        public async Task<IActionResult> Get() => Ok(await _unitOfWork.Doctor.GetAllAsync());
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var result = await _unitOfWork.Doctor.FindAsync(id);
+            if (!result.Success) return NotFound(result);
+
+            return Ok(result);
+        }
+    }
+}
