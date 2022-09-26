75cd1e6ef863b8bc607c1ae49581dda0fd7224c6
Keith Stanley <iamstan13y@gmail.com>
2022-09-26 15:43:19 +0200

    Medication -> Disease relationship fix & DB migrations.

---
+++ CimasHackathon.API/Migrations/20220926134140_MedicationDiseaseFixToDb.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CimasHackathon.API.Migrations
+{
+    public partial class MedicationDiseaseFixToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DiseaseId",
+                table: "Medications",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Diseases",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Diseases", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Medications_DiseaseId",
+                table: "Medications",
+                column: "DiseaseId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Medications_Diseases_DiseaseId",
+                table: "Medications",
+                column: "DiseaseId",
+                principalTable: "Diseases",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Medications_Diseases_DiseaseId",
+                table: "Medications");
+
+            migrationBuilder.DropTable(
+                name: "Diseases");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Medications_DiseaseId",
+                table: "Medications");
+
+            migrationBuilder.DropColumn(
+                name: "DiseaseId",
+                table: "Medications");
+        }
+    }
+}
