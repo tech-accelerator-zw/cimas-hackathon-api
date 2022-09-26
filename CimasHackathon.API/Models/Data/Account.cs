70794817d84083e9e7517c946715ef667d06834f
Keith Stanley <iamstan13y@gmail.com>
2022-09-26 11:08:09 +0200

    Patient Repository & UoW.

--- CimasHackathon.API/Models/Data/Account.cs
+++ CimasHackathon.API/Models/Data/Account.cs
@@ -7,7 +7,7 @@ namespace CimasHackathon.API.Models.Data
         public int Id { get; set; }
         public string? Email { get; set; }
         public string? Password { get; set; }
-        public string? PhoneNumber { get; set; }
+        public string? CimasNumber { get; set; }
         public DateTime DateCreated { get; set; } = DateTime.Now;
         [NotMapped]
         public string? Token { get; set; }
