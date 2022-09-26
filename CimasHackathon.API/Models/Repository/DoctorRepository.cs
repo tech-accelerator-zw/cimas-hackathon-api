d753daed30a49fc837a9cf7aa5e40db6a7fdc9d2
Keith Stanley <iamstan13y@gmail.com>
2022-09-26 23:25:32 +0200

    Doctor CRUD - I.

---
+++ CimasHackathon.API/Models/Repository/DoctorRepository.cs
@@ -0,0 +1,12 @@
+﻿using CimasHackathon.API.Models.Data;
+using CimasHackathon.API.Models.Repository.IRepository;
+
+namespace CimasHackathon.API.Models.Repository
+{
+    public class DoctorRepository : Repository<Doctor>, IDoctorRepository
+    {
+        public DoctorRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
