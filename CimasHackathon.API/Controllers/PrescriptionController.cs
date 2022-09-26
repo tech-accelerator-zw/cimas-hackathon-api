c364c17d8a34e298462cb5b7a287754dd52b9196
Keith Stanley <iamstan13y@gmail.com>
2022-09-27 00:14:46 +0200

    Get All Prescriptions.

---
+++ CimasHackathon.API/Controllers/PrescriptionController.cs
@@ -0,0 +1,20 @@
+﻿using CimasHackathon.API.Models.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CimasHackathon.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class PrescriptionController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public PrescriptionController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get() => Ok(await _unitOfWork.Prescription.GetAllAsync());
+    }
+}
