27a0c731b76f7d730afb891cd03a98359357d2d0
Keith Stanley <iamstan13y@gmail.com>
2022-09-27 00:06:55 +0200

    Prescription model & DB migrations.

---
+++ CimasHackathon.API/Models/Data/Prescription.cs
@@ -0,0 +1,16 @@
+﻿using CimasHackathon.API.Enums;
+
+namespace CimasHackathon.API.Models.Data
+{
+    public class Prescription
+    {
+        public int Id { get; set; }
+        public int PatientId { get; set; }
+        public int MedicationId { get; set; }
+        public string? Description { get; set; }
+        public PrescriptionStatus Status { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public Medication? Medication { get; set; }
+        public Patient? Patient { get; set; }
+    }
+}
