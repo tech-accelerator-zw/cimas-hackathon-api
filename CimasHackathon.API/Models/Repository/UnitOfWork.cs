c692f00836c2315b88b61fc487c9b256106514a1
Keith Stanley <iamstan13y@gmail.com>
2022-09-26 12:34:33 +0200

    Diseases CRUD.

--- CimasHackathon.API/Models/Repository/UnitOfWork.cs
+++ CimasHackathon.API/Models/Repository/UnitOfWork.cs
@@ -8,11 +8,13 @@ namespace CimasHackathon.API.Models.Repository
         private readonly AppDbContext _context;
         public IPatientRepository Patient { get; private set; }
         public IMedicationRepository Medication { get; private set; }
+        public IDiseaseRepository Disease { get; private set; }
         
         public UnitOfWork(AppDbContext context)
         {
             Patient = new PatientRepository(context);
             Medication = new MedicationRepository(context);
+            Disease = new DiseaseRepository(context);
             _context = context;
         }
 
