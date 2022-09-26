03dbcd3eb4124f88ee57da35b51ae8f9ed187a0c
Keith Stanley <iamstan13y@gmail.com>
2022-09-26 02:10:26 +0200

    Patient Account config.

---
+++ CimasHackathon.API/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using CimasHackathon.API.Models.Local;
+using CimasHackathon.API.Models.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CimasHackathon.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IAccountRepository _accountRepository;
+
+        public AccountController(IUnitOfWork unitOfWork, IAccountRepository accountRepository)
+        {
+            _unitOfWork = unitOfWork;
+            _accountRepository = accountRepository;
+        }
+
+
+        [HttpPost("login/patient")]
+        public async Task<IActionResult> Login(PatientLoginRequest request)
+        {
+            var result = await _accountRepository.PatientLoginAsync(request);
+
+            if (!result.Success)
+                return BadRequest(result);
+
+            return Ok(result);
+        }
+    }
+}
