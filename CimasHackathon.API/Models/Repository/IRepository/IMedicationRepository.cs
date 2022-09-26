b15cb1949b01e4e2ee5a5baeb16e766b16730e96
Keith Stanley <iamstan13y@gmail.com>
2022-09-26 15:55:24 +0200

    Get medication by disease Id.

--- CimasHackathon.API/Models/Repository/IRepository/IMedicationRepository.cs
+++ CimasHackathon.API/Models/Repository/IRepository/IMedicationRepository.cs
@@ -1,8 +1,10 @@
 ﻿using CimasHackathon.API.Models.Data;
+using CimasHackathon.API.Models.Local;
 
 namespace CimasHackathon.API.Models.Repository.IRepository
 {
     public interface IMedicationRepository : IRepository<Medication>
     {
+        Task<Result<IEnumerable<Medication>>> GetByDiseaseIdAsync(int diseaseId);
     }
 }
