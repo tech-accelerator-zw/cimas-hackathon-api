7abac21bca970e15d28307a41a427243b9c56070
Keith Stanley <iamstan13y@gmail.com>
2022-09-27 00:27:45 +0200

    Get Prescriptions by Patient Id.

--- CimasHackathon.API/Models/Repository/PrescriptionRepository.cs
+++ CimasHackathon.API/Models/Repository/PrescriptionRepository.cs
@@ -1,5 +1,7 @@
 ﻿using CimasHackathon.API.Models.Data;
+using CimasHackathon.API.Models.Local;
 using CimasHackathon.API.Models.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
 
 namespace CimasHackathon.API.Models.Repository
 {
@@ -7,6 +9,17 @@ namespace CimasHackathon.API.Models.Repository
     {
         public PrescriptionRepository(AppDbContext context) : base(context)
         {
+        }
+
+        public async Task<Result<IEnumerable<Prescription>>> GetByPatientIdAsync(int patientId)
+        {
+            var prescriptions = await _dbSet
+                .Where(x => x.PatientId == patientId)
+                .Include(x => x.Patient)
+                .Include(x => x.Medication)
+                .ToListAsync();
+
+            return new Result<IEnumerable<Prescription>>(prescriptions);
         }
     }
 }
