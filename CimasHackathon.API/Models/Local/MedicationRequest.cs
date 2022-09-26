66314acf366f245b6b20abf7608568f4dbfade12
Keith Stanley <iamstan13y@gmail.com>
2022-09-26 11:54:46 +0200

    Medication CRUD.

---
+++ CimasHackathon.API/Models/Local/MedicationRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace CimasHackathon.API.Models.Local
+{
+    public class MedicationRequest
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
