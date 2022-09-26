be7a3bda16ac7cf295c49950f1b15d023a733c63
Keith Stanley <iamstan13y@gmail.com>
2022-09-26 11:30:32 +0200

    Medication model & DB migrations.

---
+++ CimasHackathon.API/Models/Data/Medication.cs
@@ -0,0 +1,9 @@
+﻿namespace CimasHackathon.API.Models.Data
+{
+    public class Medication
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
