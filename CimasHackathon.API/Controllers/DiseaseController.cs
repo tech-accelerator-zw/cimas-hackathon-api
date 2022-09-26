8e2b106b9a4381c01af9086d7b3e38056120e175
Keith Stanley <iamstan13y@gmail.com>
2022-09-26 12:33:52 +0200

    Disease CRUD.

---
+++ CimasHackathon.API/Controllers/DiseaseController.cs
@@ -0,0 +1,56 @@
+﻿using CimasHackathon.API.Models.Data;
+using CimasHackathon.API.Models.Local;
+using CimasHackathon.API.Models.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CimasHackathon.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class DiseaseController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DiseaseController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(DiseaseRequest request)
+        {
+            var result = await _unitOfWork.Disease.AddAsync(new Disease
+            {
+                MedicationId = request.MedicationId,
+                Name = request.Name
+            });
+
+            if (!result.Success) return BadRequest(result);
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get() => Ok(await _unitOfWork.Disease.GetAllAsync());
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var result = await _unitOfWork.Disease.FindAsync(id);
+
+            if (!result.Success) return NotFound(result);
+
+            return Ok(result);
+        }
+
+        [HttpGet("medication/{medicationId}")]
+        public async Task<IActionResult> GetByMedicationId(int medicationId)
+        {
+            var result = await _unitOfWork.Disease.GetByMedicationIdAsync(medicationId);
+            
+            if (!result.Success) return NotFound(result);
+
+            return Ok(result);
+        }
+    }
+}
