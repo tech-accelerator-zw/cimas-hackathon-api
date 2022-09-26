1e4eda44863bc00188fafc0d49396c032f207064
Keith Stanley <iamstan13y@gmail.com>
2022-09-26 16:38:06 +0200

    Doctor model & DB migrations.

---
+++ CimasHackathon.API/Models/Data/Doctor.cs
@@ -0,0 +1,10 @@
+﻿namespace CimasHackathon.API.Models.Data
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? Location { get; set; }
+    }
+}
