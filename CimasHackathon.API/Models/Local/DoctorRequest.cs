65fec571f33abacc59210654a91277e3c7ab7f97
Keith Stanley <iamstan13y@gmail.com>
2022-09-26 23:43:59 +0200

    Add Doctor implementation.

---
+++ CimasHackathon.API/Models/Local/DoctorRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace CimasHackathon.API.Models.Local
+{
+    public class DoctorRequest
+    {
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? Location { get; set; }
+    }
+}
