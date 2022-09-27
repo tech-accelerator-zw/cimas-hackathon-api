18d5766c863b31101218f6d5281befa6135e51dd
Keith Stanley <iamstan13y@gmail.com>
2022-09-27 03:36:55 +0200

    Pharmacy login implementation.

--- CimasHackathon.API/Models/Data/Pharmacy.cs
+++ CimasHackathon.API/Models/Data/Pharmacy.cs
@@ -1,4 +1,6 @@
-﻿namespace CimasHackathon.API.Models.Data
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CimasHackathon.API.Models.Data
 {
     public class Pharmacy
     {
@@ -6,5 +8,7 @@
         public string? Name { get; set; }
         public string? Email { get; set; }
         public string? Address { get; set; }
+        [NotMapped]
+        public string? Token { get; set; }
     }
 }
