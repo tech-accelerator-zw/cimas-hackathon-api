5b65ef09ceea752dd7b3537da38b05399d5a5739
Keith Stanley <iamstan13y@gmail.com>
2022-09-27 03:14:14 +0200

    Pharmacy model & DB migrations.

---
+++ CimasHackathon.API/Models/Data/Pharmacy.cs
@@ -0,0 +1,10 @@
+﻿namespace CimasHackathon.API.Models.Data
+{
+    public class Pharmacy
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Address { get; set; }
+    }
+}
