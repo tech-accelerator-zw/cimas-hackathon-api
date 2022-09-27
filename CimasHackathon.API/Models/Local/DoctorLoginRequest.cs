ebc91cafa13ded259412c1bd72fd389d84cceaee
Keith Stanley <iamstan13y@gmail.com>
2022-09-27 02:53:19 +0200

    Doctor Login implementation.

---
+++ CimasHackathon.API/Models/Local/DoctorLoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace CimasHackathon.API.Models.Local
+{
+    public class DoctorLoginRequest
+    {
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+}
