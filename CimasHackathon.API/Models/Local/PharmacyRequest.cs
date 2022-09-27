0aafea6167fe28f04a625e23a6234625304df8bf
Keith Stanley <iamstan13y@gmail.com>
2022-09-27 03:26:28 +0200

    Add Pharmacy implementation.

---
+++ CimasHackathon.API/Models/Local/PharmacyRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace CimasHackathon.API.Models.Local
+{
+    public class PharmacyRequest
+    {
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Address { get; set; }
+    }
+}
