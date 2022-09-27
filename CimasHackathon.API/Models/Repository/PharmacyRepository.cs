a64f355c01a14164b01c8456f120b09bc52e0a43
Keith Stanley <iamstan13y@gmail.com>
2022-09-27 03:19:24 +0200

    Pharmacy Repository & UoW.

---
+++ CimasHackathon.API/Models/Repository/PharmacyRepository.cs
@@ -0,0 +1,12 @@
+﻿using CimasHackathon.API.Models.Data;
+using CimasHackathon.API.Models.Repository.IRepository;
+
+namespace CimasHackathon.API.Models.Repository
+{
+    public class PharmacyRepository : Repository<Pharmacy>, IPharmacyRepository
+    {
+        public PharmacyRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
