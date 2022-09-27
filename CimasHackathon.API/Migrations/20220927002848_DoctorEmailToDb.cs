d9be067dfd1e11c354f21fc444ad143d48ee4e4c
Keith Stanley <iamstan13y@gmail.com>
2022-09-27 02:53:01 +0200

    Doctor Login implementation.

---
+++ CimasHackathon.API/Migrations/20220927002848_DoctorEmailToDb.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CimasHackathon.API.Migrations
+{
+    public partial class DoctorEmailToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Doctors",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Doctors");
+        }
+    }
+}
