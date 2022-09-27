9647c4acc0842f030ef9a945dd30700d406062b2
Keith Stanley <iamstan13y@gmail.com>
2022-09-27 02:53:15 +0200

    Doctor Login implementation.

--- CimasHackathon.API/Models/Data/Doctor.cs
+++ CimasHackathon.API/Models/Data/Doctor.cs
@@ -1,10 +1,15 @@
-﻿namespace CimasHackathon.API.Models.Data
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CimasHackathon.API.Models.Data
 {
     public class Doctor
     {
         public int Id { get; set; }
         public string? Name { get; set; }
         public string? Surname { get; set; }
+        public string? Email { get; set; }
         public string? Location { get; set; }
+        [NotMapped]
+        public string? Token { get; set; }
     }
 }
