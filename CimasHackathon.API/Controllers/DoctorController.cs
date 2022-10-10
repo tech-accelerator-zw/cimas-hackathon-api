6c07ba62b5f2d71fe53ac1ec479fe741f2d8700a
Keith Stanley <iamstan13y@gmail.com>
2022-10-10 22:05:48 +0200

    Code Cleanup - I.

--- CimasHackathon.API/Controllers/DoctorController.cs
+++ CimasHackathon.API/Controllers/DoctorController.cs
@@ -39,7 +39,7 @@ namespace CimasHackathon.API.Controllers
             if (!result.Success) return BadRequest(result);
 
             _unitOfWork.SaveChanges();
-            
+
             return Ok(result);
         }
     }
